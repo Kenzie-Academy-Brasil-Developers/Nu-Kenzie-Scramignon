af972efaea0f04c09d23efbca40ff72ef30b363e
Fernando Scramignon Narde <scramignonnarde@gmail.com>
2022-05-27 10:24:48 -0300

    basic structure

---
+++ src/components/MainPage/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css"
+
+function MainPage() {
+    return (
+        <div className="mainPage">
+
+        </div>
+    )
+}
+
+export default MainPage;
---
+++ src/components/WelcomePage/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function WelcomePage() {
+    return (
+        <div class="welcomePage">
+            
+        </div>
+    )
+}
+
+export default WelcomePage;
